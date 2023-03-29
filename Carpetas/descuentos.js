5ff44ec82eddd4117b0a600f01023c3913cb4f88
SrPaisita <29paisa@gmail.com>
2023-03-28 20:05:08 -0500

    Solucionando descuentos

--- Carpetas/descuentos.js
+++ Carpetas/descuentos.js
@@ -1,6 +1,6 @@
 const btn = document.querySelector("#calcular");
 const inputPrice = document.querySelector("#price");
-const inputDiscount = document.querySelector("#discount");
+const inputCoupon = document.querySelector("#coupon");
 const pResult = document.querySelector("#result");
 
 
@@ -8,19 +8,19 @@ btn.addEventListener("click", calcularPrecioDescuento);
 
 function calcularPrecioDescuento() {
     const price = Number(inputPrice.value);
-    const discount =Number(inputDiscount.value);
+    const coupon = inputCoupon.value;
 
-    if(!price || !discount) {
+    if(!price || !coupon) {
         pResult.innerText = "Debes llenar el formulario!"
         return
     }
 
-    if(discount > 100) {
+    if(coupon > 100) {
         pResult.innerText = "Debes ingresar un descuento valido."
         return
     }
 
-    const newPrice = (price * (100 - discount) / 100)
+    const newPrice = (price * (100 - coupon) / 100)
 
     pResult.innerText = "El nuevo precio con descuento es $" + newPrice;
 }
